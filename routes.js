a0c246539b50e19c67094c40e8ee66f1a6e4d743
dsab123 <dsabbaghumd@gmail.com>
2016-12-21 13:51:01 -0500

    removed quotes.schedule, added route for channels.schedule

--- routes.js
+++ routes.js
@@ -4,9 +4,12 @@ var router = require('express').Router();
 var quotes = require('./controllers/quotes');
 var channels = require('./controllers/channels');
 
-router.post('/quote', quotes.create, quotes.schedule); 
+router.post('/quote', quotes.create); //, quotes.schedule); 
 
 router.post('/createChannel', channels.create);
+
+router.post('/scheduleChannel', channels.schedule);
+
 
 // once I get to these
 /*
